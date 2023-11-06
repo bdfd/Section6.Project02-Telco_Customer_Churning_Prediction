274e4aab04c25dfb7ae5026eec809e642a5774c7
David Chen <bdfd2005@gmail.com>
2023-11-06 17:34:16 -0500

    update on minor template

--- predict/predict.py
+++ predict/predict.py
@@ -2,7 +2,7 @@
 Date         : 2022-12-09 12:54:06
 Author       : BDFD,bdfd2005@gmail.com
 Github       : https://github.com/bdfd
-LastEditTime : 2023-10-26 12:49:25
+LastEditTime : 2023-11-06 16:42:43
 LastEditors  : BDFD
 Description  : 
 FilePath     : \predict\predict.py
@@ -16,55 +16,34 @@ import execdata as exe
 predict = Blueprint('predict', __name__,
                     static_folder='static', template_folder='templates')
 
-df = pd.read_csv(
-    'https://raw.githubusercontent.com/bdfd/Section6.Project01-Car-Price-Predictor/Pickle-Demo/dataset/Car_Munging_Data.csv',
-    encoding='utf-8')
-df = df.iloc[:, 1:]
-# Check the unique value in company columns
-company_lists = df['company'].unique().tolist()
-company_lists.sort()
-name_lists = df['name'].unique().tolist()
-name_lists.sort()
-year_lists = df['year'].unique().tolist()
-year_lists.sort()
-fuel_type_lists = df['fuel_type'].unique().tolist()
-fuel_type_lists.sort()
-model = temp.Car_Prediction()
+model, sample_le = temp.supervised_classification.Tele_Customer_Churn_0602()
 
 
 @predict.route('/', methods=["POST", "GET"])
 def predict_index():
-    print(company_lists)
-    mingzi = " "
-    name = " "
-    company = " "
-    year = " "
-    kms_driven = " "
-    fuel_type = " "
+    SeniorCiitizen = " "
+    Partner = " "
+    Dependents = " "
+    tenure = " "
+    OnlineSecurity = " "
+    TechSupport = " "
+    Contract = " "
+    PaperlessBilling = " "
+    PaymentMethod = " "
+    MonthlyCharges = " "
     if request.method == "GET":
-        return render_template('homepage/predict_index.html', companies=company_lists, car_models=name_lists,
-                               year_lists=year_lists, fuel_type_lists=fuel_type_lists,
-                               name=name, mingzi=mingzi, company=company, year=year,
-                               kms_driven=kms_driven, fuel_type=fuel_type)
+        return render_template('homepage/predict_index.html')
     else:
-        mingzi = request.form["mingzi"]
-        name = request.form["car_models"]
-        company = request.form["company"]
-        year = request.form["year"]
-        year = exe.convint(year)
-        kms_driven = request.form["kms_driven"]
-        kms_driven = exe.convint(kms_driven)
-        fuel_type = request.form["fuel_type"]
-        # print(type(mingzi), type(model), type(company),
-        #       type(year), type(kms_driven), type(fuel_type))
-        # print(name, company, year, kms_driven, fuel_type)
-        prediction = model.predict(pd.DataFrame(columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'],
-                                                data=np.array([name, company, year, kms_driven, fuel_type]).reshape(1, 5)))
-        result = str(np.round(prediction[0], 2))
-        print(result)
-        return render_template('homepage/predict_index.html', result=result, name=name, mingzi=mingzi, company=company, year=year, kms_driven=kms_driven, fuel_type=fuel_type)
-
-
-# @predict.route('/home')
-# def predict_home():
-#     return render_template('predict_index.html')
+        para_list = []
+        SeniorCiitizen = para_list.append(request.form["SeniorCiitizen"])
+        Partner = para_list.append(request.form["Partner"])
+        Dependents = para_list.append(request.form["Dependents"])
+        tenure = para_list.append(exe.convint(request.form["tenure"]))
+        OnlineSecurity = para_list.append(request.form["OnlineSecurity"])
+        TechSupport = para_list.append(request.form["TechSupport"])
+        Contract = para_list.append(request.form["Contract"])
+        PaperlessBilling = para_list.append(request.form["PaperlessBilling"])
+        PaymentMethod = para_list.append(request.form["PaymentMethod"])
+        MonthlyCharges = para_list.append(request.form["MonthlyCharges"])
+        print(para_list)
+        return render_template('homepage/predict_index.html')
