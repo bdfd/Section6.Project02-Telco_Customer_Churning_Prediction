9148a4a280968415d2bf9793f5037731cbf65646
David Chen <bdfd2005@gmail.com>
2023-11-08 14:23:35 -0500

    update prediction page

--- predict/predict.py
+++ predict/predict.py
@@ -2,7 +2,7 @@
 Date         : 2022-12-09 12:54:06
 Author       : BDFD,bdfd2005@gmail.com
 Github       : https://github.com/bdfd
-LastEditTime : 2023-11-07 12:19:32
+LastEditTime : 2023-11-08 14:22:30
 LastEditors  : BDFD
 Description  : 
 FilePath     : \predict\predict.py
@@ -17,11 +17,10 @@ predict = Blueprint('predict', __name__,
                     static_folder='static', template_folder='templates')
 
 df = pd.read_csv(
-    'https://raw.githubusercontent.com/bdfd/Section6.Project02-Telco_Customer_Churning_Prediction/main/1.0%20dataset/S602_Preprocessed.csv',
+    'https://raw.githubusercontent.com/bdfd/Section6.Project02-Telco_Customer_Churning_Prediction/main/1.0%20dataset/S602_Preprocessed_Data.csv',
     encoding='utf-8')
 seniorcitizen_lists = df['SeniorCitizen'].unique().tolist()
 seniorcitizen_lists.sort()
-print(seniorcitizen_lists)
 partner_lists = df['Partner'].unique().tolist()
 partner_lists.sort()
 dependents_lists = df['Dependents'].unique().tolist()
@@ -51,32 +50,44 @@ def predict_index():
     PaymentMethod = " "
     MonthlyCharges = " "
     if request.method == "GET":
-        return render_template('homepage/predict_index.html', seniorcitizen_lists=seniorcitizen_lists,
-                               partner_lists=partner_lists, dependents_lists=dependents_lists,
-                               onlinesecurity_lists=onlinesecurity_lists, techsupport_lists=techsupport_lists,
-                               contract_lists=contract_lists, paperlessbilling_lists=paperlessbilling_lists,
-                               paymentmethod_lists=paymentmethod_lists)
+        return render_template('homepage/predict_index.html',SeniorCitizen=SeniorCitizen,
+                                Partner=Partner, Dependents=Dependents, tenure=tenure, OnlineSecurity=OnlineSecurity,
+                                TechSupport=TechSupport, Contract=Contract, PaperlessBilling=PaperlessBilling,
+                                PaymentMethod=PaymentMethod, MonthlyCharges=MonthlyCharges,
+                                seniorcitizen_lists=seniorcitizen_lists,
+                                partner_lists=partner_lists, dependents_lists=dependents_lists,
+                                onlinesecurity_lists=onlinesecurity_lists, techsupport_lists=techsupport_lists,
+                                contract_lists=contract_lists, paperlessbilling_lists=paperlessbilling_lists,
+                                paymentmethod_lists=paymentmethod_lists)
     else:
         para_list = []
-        SeniorCitizen = para_list.append(request.form["SeniorCitizen"])
-        Parnter = para_list.append(request.form["Parnter"])
-        Dependents = para_list.append(request.form["Dependents"])
-        tenure = para_list.append(exe.convint(request.form["tenure"]))
-        OnlineSecurity = para_list.append(request.form["OnlineSecurity"])
-        TechSupport = para_list.append(request.form["TechSupport"])
-        Contract = para_list.append(request.form["Contract"])
-        PaperlessBilling = para_list.append(request.form["PaperlessBilling"])
-        PaymentMethod = para_list.append(request.form["PaymentMethod"])
-        MonthlyCharges = para_list.append(request.form["MonthlyCharges"])
+        SeniorCitizen = request.form["SeniorCitizen"]
+        para_list.append(SeniorCitizen)
+        Partner = request.form["Partner"]
+        para_list.append(Partner)
+        Dependents = request.form["Dependents"]
+        para_list.append(Dependents)
+        tenure = exe.convint(request.form["tenure"])
+        para_list.append(tenure)
+        OnlineSecurity = request.form["OnlineSecurity"]
+        para_list.append(OnlineSecurity)
+        TechSupport = request.form["TechSupport"]
+        para_list.append(TechSupport)
+        Contract = request.form["Contract"]
+        para_list.append(Contract)
+        PaperlessBilling = request.form["PaperlessBilling"]
+        para_list.append(PaperlessBilling)
+        PaymentMethod = request.form["PaymentMethod"]
+        para_list.append(PaymentMethod)
+        MonthlyCharges = request.form["MonthlyCharges"]
+        para_list.append(MonthlyCharges)
         result = temp.supervised_classification.Tele_Customer_Churn_0602(
             para_list)
         if result:
             result = 'Will Be Churned.'
         else:
             result = 'Will Be Stay With Us.'
-        return render_template('homepage/predict_index.html', SeniorCitizen=SeniorCitizen,
-                               Partner=Partner, Dependents=Dependents, tenure=tenure,
-                               OnlineSecurity=OnlineSecurity, TechSupport=TechSupport,
-                               Contract=Contract, PaperlessBilling=PaperlessBilling,
-                               PaymentMethod=PaymentMethod, MonthlyCharges=MonthlyCharges,
-                               result=result)
+        return render_template('homepage/predict_index.html', result=result, SeniorCitizen=SeniorCitizen,
+                                Partner=Partner, Dependents=Dependents, tenure=tenure, OnlineSecurity=OnlineSecurity,
+                                TechSupport=TechSupport, Contract=Contract, PaperlessBilling=PaperlessBilling,
+                                PaymentMethod=PaymentMethod, MonthlyCharges=MonthlyCharges)
